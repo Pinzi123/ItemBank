3ea55175efaf4344ebce0404d0a166b4fccd204a
Pinzi123 <995537534@qq.com>
2017-10-19 11:08:04 +0800

    simple page

---
+++ data/items.js
@@ -0,0 +1,24 @@
+var itemsA = {
+	"errNo":"0",
+	"content": [
+	    {
+			"topic":"求`x^2+3x-4=0`中x的值：",
+			"type":"radio",
+			"options":["4","-4","4和-1","-4和1"],
+			"correct":"3"
+		},
+		{
+		    "topic":"设事件A,B相互独立，且已知`P(A)=0,P(A\cupB)=0.7`，则P(B) = ( )",
+			"type":"radio",
+			"options":["1/16","1/4","1/10","4/10"],
+			"correct":"3"
+		},	
+		{
+		    "topic":"题目三：选A",
+			"type":"radio",
+			"options":["A","B","C","D"],
+			"correct":"3"
+		}	
+	],
+	"length":3
+}
--- js/topic.js
+++ js/topic.js
@@ -1,6 +1,31 @@
 window.onload = function () {
+ //请求数据,可以下载一个模拟http的插件
+ /**
+ var xhr = new XMLHttpRequest()
+
+ xhr.open("GET","./data/items.js",false); 
+ 
+ xhr.onreadystatechange = function () {
+	 if (xhr.readyState == 4){
+		 if (xhr.status ==200) {
+			 alert("OK")
+		 }
+	 }
+ }
+ xhr.send();**/
+ 
  var topic = document.getElementsByClassName('topic')[0];
  var icons = document.getElementsByClassName('icon')
+ var problem = document.getElementById('problem')
+ var options = topic.getElementsByClassName('option')
+ var pages = document.getElementById('pages')
+ var pageSpan
+ 
+ // 渲染数据
+ resetTopic(1,itemsA);
+ resetPages(itemsA.length)
+
+ // 绑定事件
  
  bindEvent(topic, 'mouseover', 'radio', function(e){
 	 e.target.className = e.target.className + ' checked';
@@ -18,16 +43,58 @@ window.onload = function () {
 	 }
 	  e.target.getElementsByClassName('icon')[0].className =  e.target.getElementsByClassName('icon')[0].className + ' selected';
   })
- 
- 
+  
+  bindEvent(pages, 'click', 'SPAN', function(e) {
+	 for(var i = 0; i < pageSpan.length; i++){
+		 removeClassName(pageSpan[i],' checked');
+	 }
+	 checked(e.target)
+  })
+  
+  
+  /** 内置函数 **/
+  
+  //重置题目
+  function resetTopic(num,items){
+	 // 清除样式
+	 for(var i = 0; i < icons.length; i++){
+		 removeClassName(icons[i],' selected');
+	 } 
+	 // 更改题目内容
+	 var content = items.content[num]
+	 problem.innerHTML = content.topic
+	 // 更改选项内容
+	 for(var i = 0; i < options.length; i++){
+		 options[i].innerHTML = content.options[i]
+	 }
+  }
+  
+  // 设置题目数
+  function resetPages(len) {
+	  for(var i=0; i<len; i++){
+		  var span = document.createElement("span");
+		  var node=document.createTextNode(i+1);
+          span.appendChild(node);
+		  pages.appendChild(span);
+	  }
+	  pageSpan = pages.getElementsByTagName("span")
+	  console.log(pageSpan.length)
+	  checked(pageSpan[0])
+  }
  
 }
+
+
+/** 全局函数 **/
 
 // 移除类名
 function removeClassName(e, className){
 	e.className = e.className.replace(className,"")
 }
-
+//选中 
+function checked(node) {
+	node.className = node.className + " checked"
+}
 //事件代理
 function bindEvent(elem, type, selector, fn){
 	if(fn == null){
@@ -38,7 +105,7 @@ function bindEvent(elem, type, selector, fn){
 		var target
 		if(selector&&e.target !== elem){
 			target = e.target
-			if(target.className.match(selector)) {
+			if(target.className.match(selector) || target.nodeName.match(selector)) {
 				fn.call(target, e)
 			}
 		} else {
